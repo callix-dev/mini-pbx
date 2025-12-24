d8b7f068eb65591cd38aa802652cac714badda8c
office@callix.ai <office@callix.ai>
2025-12-25 04:58:15 +0530

    timezone standardisation

--- resources/views/platform/users/edit.blade.php
+++ resources/views/platform/users/edit.blade.php
@@ -98,17 +98,65 @@
                         <div>
                             <label for="timezone" class="form-label">Timezone</label>
                             <select name="timezone" id="timezone" class="form-select">
-                                <option value="UTC" {{ old('timezone', $user->timezone) === 'UTC' ? 'selected' : '' }}>UTC</option>
-                                <option value="America/New_York" {{ old('timezone', $user->timezone) === 'America/New_York' ? 'selected' : '' }}>Eastern Time (US)</option>
-                                <option value="America/Chicago" {{ old('timezone', $user->timezone) === 'America/Chicago' ? 'selected' : '' }}>Central Time (US)</option>
-                                <option value="America/Denver" {{ old('timezone', $user->timezone) === 'America/Denver' ? 'selected' : '' }}>Mountain Time (US)</option>
-                                <option value="America/Los_Angeles" {{ old('timezone', $user->timezone) === 'America/Los_Angeles' ? 'selected' : '' }}>Pacific Time (US)</option>
-                                <option value="Europe/London" {{ old('timezone', $user->timezone) === 'Europe/London' ? 'selected' : '' }}>London</option>
-                                <option value="Europe/Paris" {{ old('timezone', $user->timezone) === 'Europe/Paris' ? 'selected' : '' }}>Paris</option>
-                                <option value="Asia/Tokyo" {{ old('timezone', $user->timezone) === 'Asia/Tokyo' ? 'selected' : '' }}>Tokyo</option>
-                                <option value="Asia/Shanghai" {{ old('timezone', $user->timezone) === 'Asia/Shanghai' ? 'selected' : '' }}>Shanghai</option>
-                                <option value="Asia/Kolkata" {{ old('timezone', $user->timezone) === 'Asia/Kolkata' ? 'selected' : '' }}>India (IST)</option>
-                                <option value="Australia/Sydney" {{ old('timezone', $user->timezone) === 'Australia/Sydney' ? 'selected' : '' }}>Sydney</option>
+                                @php
+                                    $selectedTimezone = old('timezone', $user->timezone ?? 'UTC');
+                                    $timezones = [
+                                        'UTC' => ['name' => 'UTC', 'offset' => '+00:00'],
+                                        'Pacific/Midway' => ['name' => 'SST', 'offset' => '-11:00'],
+                                        'Pacific/Honolulu' => ['name' => 'HST', 'offset' => '-10:00'],
+                                        'America/Anchorage' => ['name' => 'AKST', 'offset' => '-09:00'],
+                                        'America/Los_Angeles' => ['name' => 'PST', 'offset' => '-08:00'],
+                                        'America/Denver' => ['name' => 'MST', 'offset' => '-07:00'],
+                                        'America/Chicago' => ['name' => 'CST', 'offset' => '-06:00'],
+                                        'America/New_York' => ['name' => 'EST', 'offset' => '-05:00'],
+                                        'America/Caracas' => ['name' => 'VET', 'offset' => '-04:00'],
+                                        'America/Halifax' => ['name' => 'AST', 'offset' => '-04:00'],
+                                        'America/St_Johns' => ['name' => 'NST', 'offset' => '-03:30'],
+                                        'America/Sao_Paulo' => ['name' => 'BRT', 'offset' => '-03:00'],
+                                        'America/Argentina/Buenos_Aires' => ['name' => 'ART', 'offset' => '-03:00'],
+                                        'Atlantic/South_Georgia' => ['name' => 'GST', 'offset' => '-02:00'],
+                                        'Atlantic/Azores' => ['name' => 'AZOT', 'offset' => '-01:00'],
+                                        'Europe/London' => ['name' => 'GMT', 'offset' => '+00:00'],
+                                        'Europe/Paris' => ['name' => 'CET', 'offset' => '+01:00'],
+                                        'Europe/Berlin' => ['name' => 'CET', 'offset' => '+01:00'],
+                                        'Africa/Lagos' => ['name' => 'WAT', 'offset' => '+01:00'],
+                                        'Africa/Cairo' => ['name' => 'EET', 'offset' => '+02:00'],
+                                        'Africa/Johannesburg' => ['name' => 'SAST', 'offset' => '+02:00'],
+                                        'Europe/Athens' => ['name' => 'EET', 'offset' => '+02:00'],
+                                        'Asia/Jerusalem' => ['name' => 'IST', 'offset' => '+02:00'],
+                                        'Europe/Moscow' => ['name' => 'MSK', 'offset' => '+03:00'],
+                                        'Asia/Kuwait' => ['name' => 'AST', 'offset' => '+03:00'],
+                                        'Africa/Nairobi' => ['name' => 'EAT', 'offset' => '+03:00'],
+                                        'Asia/Tehran' => ['name' => 'IRST', 'offset' => '+03:30'],
+                                        'Asia/Dubai' => ['name' => 'GST', 'offset' => '+04:00'],
+                                        'Asia/Kabul' => ['name' => 'AFT', 'offset' => '+04:30'],
+                                        'Asia/Karachi' => ['name' => 'PKT', 'offset' => '+05:00'],
+                                        'Asia/Kolkata' => ['name' => 'IST', 'offset' => '+05:30'],
+                                        'Asia/Kathmandu' => ['name' => 'NPT', 'offset' => '+05:45'],
+                                        'Asia/Dhaka' => ['name' => 'BST', 'offset' => '+06:00'],
+                                        'Asia/Yangon' => ['name' => 'MMT', 'offset' => '+06:30'],
+                                        'Asia/Bangkok' => ['name' => 'ICT', 'offset' => '+07:00'],
+                                        'Asia/Jakarta' => ['name' => 'WIB', 'offset' => '+07:00'],
+                                        'Asia/Shanghai' => ['name' => 'CST', 'offset' => '+08:00'],
+                                        'Asia/Singapore' => ['name' => 'SGT', 'offset' => '+08:00'],
+                                        'Asia/Hong_Kong' => ['name' => 'HKT', 'offset' => '+08:00'],
+                                        'Australia/Perth' => ['name' => 'AWST', 'offset' => '+08:00'],
+                                        'Asia/Tokyo' => ['name' => 'JST', 'offset' => '+09:00'],
+                                        'Asia/Seoul' => ['name' => 'KST', 'offset' => '+09:00'],
+                                        'Australia/Adelaide' => ['name' => 'ACST', 'offset' => '+09:30'],
+                                        'Australia/Sydney' => ['name' => 'AEST', 'offset' => '+10:00'],
+                                        'Pacific/Guam' => ['name' => 'ChST', 'offset' => '+10:00'],
+                                        'Pacific/Noumea' => ['name' => 'NCT', 'offset' => '+11:00'],
+                                        'Pacific/Auckland' => ['name' => 'NZST', 'offset' => '+12:00'],
+                                        'Pacific/Fiji' => ['name' => 'FJT', 'offset' => '+12:00'],
+                                        'Pacific/Tongatapu' => ['name' => 'TOT', 'offset' => '+13:00'],
+                                    ];
+                                @endphp
+                                @foreach($timezones as $tz => $info)
+                                    <option value="{{ $tz }}" {{ $selectedTimezone === $tz ? 'selected' : '' }}>
+                                        {{ $info['name'] }} (UTC{{ $info['offset'] }})
+                                    </option>
+                                @endforeach
                             </select>
                         </div>
                     </div>
